b1c49c928ff56ecf8a8400f504f1215d0df01a9f
NhatHuyDev <lenhathuy9a6@gmail.com>
2025-12-29 15:14:03 +0700

    add new

---
+++ src/test/java/vn/back_end_best_practice/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.back_end_best_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
